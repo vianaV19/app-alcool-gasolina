9ebc63ba8d72c8b41be6f3925c3472b302218dfe
vianav19 <matheus.viana518062@gmail.com>
2023-03-10 19:35:16 -0300

    changes

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -7,6 +7,24 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
+  alcool!:number; gasolina!:number; resultado:string = "Resultado!";
+
+  km!:number; precoMedio!:number; resKmRodado:string = "Resultado: R$0,00" ;
+
   constructor() {}
 
+  calcular(){
+    
+    if(this.alcool == null || this.gasolina == null) {alert("Por favor insira algo!"); return;}
+
+    let res = this.alcool / this.gasolina;
+
+    this.resultado = res < 0.7 ? "Melhor utilizar gasolina." : "Melhor utilizar alcool!";
+  }
+
+  calcularKmRodados(){
+    let res = this.precoMedio / this.km;
+    
+    this.resKmRodado = `Resultado: R$ ${res.toFixed(2)}`
+  }
 }
