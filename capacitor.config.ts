efe9454746283ffb466b071035cdba928485836d
vianav19 <matheus.viana518062@gmail.com>
2023-03-10 17:55:02 -0300

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'app-alcool-gasolina',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
